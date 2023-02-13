23e76d5d5d21051814b616ae99bda9f85c4f7efc
MonicaAuditore <mauditore@live.it>
2023-02-13 15:34:38 +0100

    init_inizializzazione progetto

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: "AppHeader",
+  data() {
+    return {
+      test: "Hello",
+    };
+  },
+};
+</script>
+
+<template>
+  <p>{{ test }}</p>
+</template>
+
+<style scoped></style>
