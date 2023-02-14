1673b090d3a595e3d9180f5b98fdf6cf039feccd
MonicaAuditore <mauditore@live.it>
2023-02-14 10:50:36 +0100

    add_stuttura about

--- src/components/SectionAbout.vue
+++ src/components/SectionAbout.vue
@@ -10,7 +10,36 @@ export default {
 </script>
 
 <template>
-  <p>SectionAbout</p>
+  <div class="mainAbout">
+    <div class="about">
+      <div class="aboutImg"></div>
+      <div class="aboutText"></div>
+      <div class="aboutTextCta"></div>
+    </div>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.about {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.aboutImg {
+  width: 60%;
+  height: 700px;
+  background-color: aquamarine;
+}
+
+.aboutText {
+  width: 40%;
+  height: 700px;
+  background-color: pink;
+}
+
+.aboutTextCta {
+  width: 90%;
+  height: 260px;
+  background-color: red;
+}
+</style>
