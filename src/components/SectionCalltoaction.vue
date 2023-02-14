e9aabd5cd9bd1b1a524c7775f0188cfe0b8a2a2c
MonicaAuditore <mauditore@live.it>
2023-02-14 14:50:59 +0100

    add_cta

--- src/components/SectionCalltoaction.vue
+++ src/components/SectionCalltoaction.vue
@@ -2,15 +2,40 @@
 export default {
   name: "SectionCalltoaction",
   data() {
-    return {
-      test: "Hello",
-    };
+    return {};
   },
 };
 </script>
 
 <template>
-  <p>SectionCalltoaction</p>
+  <div class="mainCallToAction">
+    <div class="boxCta">
+      <div class="ctaContent">
+        <h2>LET'S TALK ABOUT WORK</h2>
+        <p>
+          A wonderful serenity has taken possession far far<br />
+          away, behind the word mountains.
+        </p>
+        <button><h6>GET STARTED</h6></button>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.boxCta {
+  height: 470px;
+  background-image: url(../images/business-people-working-together-on-project-and-B3MZ4TX.jpg);
+  background-position-y: bottom;
+  background-repeat: no-repeat;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.ctaContent p {
+  color: #fff;
+  padding-bottom: 30px;
+}
+</style>
