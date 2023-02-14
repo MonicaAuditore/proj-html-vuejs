5b1052494439bc68934d6b9ef61941e8c15cace9
MonicaAuditore <mauditore@live.it>
2023-02-14 12:49:33 +0100

    add_testimonials hover

--- src/components/AppTestimonials.vue
+++ src/components/AppTestimonials.vue
@@ -3,7 +3,26 @@ export default {
   name: "AppTestimonials",
   data() {
     return {
-      test: "Hello",
+      testimonials: [
+        {
+          desc: "When, while lovely valley teems with vapour around meand eridian sun strikes the upper impenetrable foliage of my trees, and but a thousand unknow plants are noticed when i hear the buzz of the little",
+          autore: "Vera Duncan",
+          azienda: "Amazon Inc.",
+          img: "../images/oliver-ragfelt-488196-2.jpg",
+        },
+
+        {
+          desc: "When, while lovely valley teems with vapour around meand eridian sun strikes the upper impenetrable foliage of my trees, and but a thousand unknow plants are noticed when i hear the buzz of the little",
+          autore: "Vera Duncan",
+          azienda: "Amazon Inc.",
+        },
+
+        {
+          desc: "When, while lovely valley teems with vapour around meand eridian sun strikes the upper impenetrable foliage of my trees, and but a thousand unknow plants are noticed when i hear the buzz of the little",
+          autore: "Vera Duncan",
+          azienda: "Amazon Inc.",
+        },
+      ],
     };
   },
 };
@@ -20,7 +39,15 @@ export default {
       </div>
       <h2>WHAT'S THE CUSTOMER SAY<br />ABOUT OUR WORK</h2>
     </div>
-    <div class="allTestimonials"></div>
+    <div class="allTestimonials">
+      <div class="testimonial" v-for="testimonial in testimonials">
+        <div class="text">
+          <p>{{ testimonial.desc }}</p>
+          <h4>{{ testimonial.autore }}</h4>
+          <h6>{{ testimonial.azienda }}</h6>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -45,11 +72,10 @@ h2 {
 
 .allTestimonials {
   width: 90%;
-  height: 260px;
   margin-left: 10%;
   background-color: #ffffff;
   display: flex;
-  justify-content: flex-end;
+  justify-content: space-around;
   align-items: center;
 }
 
@@ -61,5 +87,25 @@ h2 {
 .titleSmallSpecial {
   text-align: center;
   color: white;
+}
+
+.testimonial {
+  width: 20%;
+  padding: 30px;
+
+  &:hover {
+    background-image: url(../images/oliver-ragfelt-488196-2.jpg);
+    background-position: center;
+    color: #fff;
+  }
+}
+
+h4 {
+  padding-top: 20px;
+  font-weight: bold;
+}
+
+h6 {
+  letter-spacing: 0;
 }
 </style>
